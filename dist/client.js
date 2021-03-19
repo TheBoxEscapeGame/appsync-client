d9795ce3d58af936c5ee3c7a1107f04becb68327
Philippe Muller <philippe@the-box.fr>
2021-03-19 10:00:59 +0100

    Update JS

--- dist/client.js
+++ dist/client.js
@@ -18,19 +18,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.fromAwsEnvironmentVariables = exports.create = void 0;
+const cross_fetch_1 = __importDefault(require("cross-fetch"));
 const core_1 = require("@apollo/client/core");
 const authLink_1 = require("./authLink");
 const options = __importStar(require("./auth/options"));
-function create({ uri, region, auth, fetch, cache = new core_1.InMemoryCache(), }) {
+function create({ uri, region, auth, fetch = cross_fetch_1.default, cache = new core_1.InMemoryCache(), }) {
     const authLink = new authLink_1.AuthLink({ region, auth, url: uri });
     const httpLink = core_1.createHttpLink({ uri, fetch });
     const link = core_1.ApolloLink.from([authLink, httpLink]);
     return new core_1.ApolloClient({ link, cache });
 }
 exports.create = create;
-function fromAwsEnvironmentVariables({ uri, region, fetch, cache = new core_1.InMemoryCache(), }) {
+function fromAwsEnvironmentVariables({ uri, region, fetch = cross_fetch_1.default, cache = new core_1.InMemoryCache(), }) {
     return create({
         uri,
         region,
